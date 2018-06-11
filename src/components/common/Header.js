dab634603482eb328ad2ba6e011aedfa5a90f9da
Bertwin <bertwinr2@gmail.com>
2018-06-11 21:01:07 +0800

    test deploy

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,7 +7,17 @@ const Header = () => {
 
         <nav className="navbar navbar-default">
             <div className="container">
-
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <Link to={`/users`}>
+                        <a className="navbar-brand" href="#">Oddle Frontend Challenge</a>
+                    </Link>
+                </div>
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
                 </div>
