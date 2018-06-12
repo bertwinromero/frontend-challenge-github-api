44a70adebb3a8a888c48c706a1b9d10d0370d2f7
Bertwin <bertwinr2@gmail.com>
2018-06-12 11:34:06 +0800

    fix(): remove toggle

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,12 +8,6 @@ const Header = () => {
         <nav className="navbar navbar-default">
             <div className="container">
                 <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
                     <Link className="navbar-brand" to={`/users`}>
                         Oddle Frontend Challenge
                     </Link>
